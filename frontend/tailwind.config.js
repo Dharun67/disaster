017f47069e61db3bae642c45a645fd46a4030100
Dharunkumar <dharunkumar.n2024cse@sece.ac.in>
2026-03-27 18:14:32 +0530

    config: Setup Tailwind CSS configuration

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        danger: '#F44336',
+        warning: '#FF9800',
+        success: '#4CAF50',
+        info: '#2196F3'
+      }
+    },
+  },
+  plugins: [],
+}
